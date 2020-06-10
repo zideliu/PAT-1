be50f43b8948fc01de370f78e3e2810a2c5540be
zideliu <34944964+zideliu@users.noreply.github.com>
2020-06-10 11:30:38 +0800

    Update 1029. Median (25).cpp

--- AdvancedLevel_C++/1029. Median (25).cpp
+++ AdvancedLevel_C++/1029. Median (25).cpp
@@ -19,3 +19,42 @@ int main() {
     printf("%d", ans);
     return 0;
 }
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+const int minn=-99999;
+void findmid(int nums1[],int nums2[],int m,int n){
+    int left = 0, right = m;
+    int totalleft=(m+n+1)/2;
+    while(left<right){
+        int i=left+(right-left+1)/2;
+        int j=totalleft-i;
+        if(nums1[i-1]>nums2[j]){
+            right=i-1;
+        }else{
+            left=i;
+        }
+    }
+    int i=left;
+    int j=totalleft-i;
+    int nums1LeftMax = nums1[i - 1];
+    int nums2LeftMax = nums2[j - 1];
+    cout<<max(nums1LeftMax,nums2LeftMax);
+}
+const int maxn=200001;
+int a[maxn];
+int b[maxn];
+int main(int argc, const char * argv[]) {
+    int n,m;
+    cin>>n;
+    for(int i=0;i<n;i++)scanf("%d",a+i);
+    cin>>m;
+    for(int i=0;i<m;i++)scanf("%d",b+i);
+    if(n>m){
+        findmid(b,a,m,n);
+    }else{
+        findmid(a,b,n,m);
+    }
+    return 0;
+}
